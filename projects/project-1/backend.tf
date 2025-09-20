591860afdb0aebf5d96931900969ec1b83c2854e
Sudheer Kumar <147365080+SudheerKumarP0357@users.noreply.github.com>
2025-09-20 15:11:50 +0530

    Update backend.tf

--- projects/project-1/backend.tf
+++ projects/project-1/backend.tf
@@ -3,6 +3,6 @@ terraform {
     container_name       = "terraform-state"
     storage_account_name = "backendterraform6"
     key                  = "terraform.tfstate"
-    resource_group_name = "backend-rg"
+    resource_group_name  = "backend-rg"
   }
 }
