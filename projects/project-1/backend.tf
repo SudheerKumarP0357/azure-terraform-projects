0c46033cebafb7d0c9ab94fcc9f5d1eb45569ce0
Sudheer Pullareddygari <spullareddygari@streamlinehealthcare.com>
2025-10-20 15:53:39 +0530

    Reset the repo

--- projects/project-1/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    container_name       = "terraform-state"
-    storage_account_name = "backendterraform6"
-    key                  = "terraform.tfstate"
-    resource_group_name  = "backend-rg"
-  }
-}
